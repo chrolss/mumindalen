fc9c96a9f9f8eb81e8adc5067d8adb2054e03d35
chrolss <chrolss@kth.se>
2016-11-16 20:50:38 +0100

    use gitignore

--- weather.php
+++ weather.php
@@ -20,8 +20,18 @@
     $json_string2 = file_get_contents("http://api.wunderground.com/api/3cb4a366870aa8ef/hourly/q/SE/Stockholm.json");
     $parsed_json = json_decode($json_string2);
     //$location = $parsed_json->{'location'}->{'city'};
-    $temp_c = $parsed_json->{'hourly_forecast'}->{'FCTTIME'}->{'hour'};
-    echo "Current temperature in Stockholm at 21 is: ${temp_c}\n";
+    $time_1 = $parsed_json->hourly_forecast[0]->{'FCTTIME'}->{'hour'};
+    $temp_1 = $parsed_json->hourly_forecast[0]->{'temp'}->{'metric'};
+    $prog_1 = $parsed_json->hourly_forecast[0]->{'wx'};
+    $time_2 = $parsed_json->hourly_forecast[1]->{'FCTTIME'}->{'hour'};
+    $temp_2 = $parsed_json->hourly_forecast[1]->{'temp'}->{'metric'};
+    $prog_2 = $parsed_json->hourly_forecast[1]->{'wx'};
+    $time_3 = $parsed_json->hourly_forecast[2]->{'FCTTIME'}->{'hour'};
+    $temp_3 = $parsed_json->hourly_forecast[2]->{'temp'}->{'metric'};
+    $prog_3 = $parsed_json->hourly_forecast[2]->{'wx'};
+    echo "The weather in Stockholm at $time_1:00 is $prog_1 and temperature: ${temp_1}\n";
+    echo "The weather in Stockholm at $time_2:00 is $prog_2 and temperature: ${temp_2}\n";
+    echo "The weather in Stockholm at $time_3:00 is $prog_3 and temperature: ${temp_3}\n";
   ?>
 
 </body>
