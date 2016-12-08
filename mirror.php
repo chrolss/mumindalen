f7027e3b01583467cba06b0b0cf064c505235090
chrolss <chrolss@kth.se>
2016-12-08 20:05:26 +0100

    here is the new code

--- mirror.php
+++ mirror.php
@@ -18,12 +18,18 @@
 		$time_1 = $parsed_json->hourly_forecast[0]->{'FCTTIME'}->{'hour'};
 		$temp_1 = $parsed_json->hourly_forecast[0]->{'temp'}->{'metric'};
 		$prog_1 = $parsed_json->hourly_forecast[0]->{'wx'};
-		$time_2 = $parsed_json->hourly_forecast[1]->{'FCTTIME'}->{'hour'};
-		$temp_2 = $parsed_json->hourly_forecast[1]->{'temp'}->{'metric'};
-		$prog_2 = $parsed_json->hourly_forecast[1]->{'wx'};
-		$time_3 = $parsed_json->hourly_forecast[2]->{'FCTTIME'}->{'hour'};
-		$temp_3 = $parsed_json->hourly_forecast[2]->{'temp'}->{'metric'};
-		$prog_3 = $parsed_json->hourly_forecast[2]->{'wx'};
+		$time_2 = $parsed_json->hourly_forecast[2]->{'FCTTIME'}->{'hour'};
+		$temp_2 = $parsed_json->hourly_forecast[2]->{'temp'}->{'metric'};
+		$prog_2 = $parsed_json->hourly_forecast[2]->{'wx'};
+		$time_3 = $parsed_json->hourly_forecast[4]->{'FCTTIME'}->{'hour'};
+		$temp_3 = $parsed_json->hourly_forecast[4]->{'temp'}->{'metric'};
+		$prog_3 = $parsed_json->hourly_forecast[4]->{'wx'};
+		$time_4 = $parsed_json->hourly_forecast[6]->{'FCTTIME'}->{'hour'};
+		$temp_4 = $parsed_json->hourly_forecast[6]->{'temp'}->{'metric'};
+		$prog_4 = $parsed_json->hourly_forecast[6]->{'wx'};
+		$time_5 = $parsed_json->hourly_forecast[8]->{'FCTTIME'}->{'hour'};
+		$temp_5 = $parsed_json->hourly_forecast[8]->{'temp'}->{'metric'};
+		$prog_5 = $parsed_json->hourly_forecast[8]->{'wx'};
 		?>
   <div class="container">
     <div class="weather-container">
@@ -33,34 +39,34 @@
       </article>
       <div class="weather-forecast">
         <div class="weather-forecast-1">
-          <img src="sun.png" alt="sun" style="vertical-align: middle;"/>
-          <span style="vertical-align: middle;"> 11 &deg; </span>
+          <img src="sun.png" alt=<?php echo "$prog_2" ?> style="vertical-align: middle;"/>
+          <span style="vertical-align: middle;"> <?php echo "$temp_2" ?> &deg; </span>
         </div>
         <div class="weather-forecast-2">
-          <img src="sun.png" alt="sun" style="vertical-align: middle;"/>
-          <span style="vertical-align: middle;"> 18 &deg; </span>
+          <img src="sun.png" alt=<?php echo "$prog_3" ?> style="vertical-align: middle;"/>
+          <span style="vertical-align: middle;"> <?php echo "$temp_3" ?> &deg; </span>
         </div>
         <div class="weather-forecast-3">
-          <img src="thunderstorm.png" alt="thunderstorm" style="vertical-align: middle;"/>
-          <span style="vertical-align: middle;"> -6 &deg; </span>
+          <img src="thunderstorm.png" alt=<?php echo "$prog_4" ?> style="vertical-align: middle;"/>
+          <span style="vertical-align: middle;"> <?php echo "$temp_4" ?> &deg; </span>
         </div>
         <div class="weather-forecast-4">
-          <img src="thunderstorm.png" alt="thunderstorm" style="vertical-align: middle;"/>
-          <span style="vertical-align: middle;"> -18 &deg; </span>
+          <img src="thunderstorm.png" alt=<?php echo "$prog_5" ?> style="vertical-align: middle;"/>
+          <span style="vertical-align: middle;"> <?php echo "$temp_5" ?> &deg; </span>
         </div>
       </div>
       <div class="weather-forecast-time">
         <div class="weather-forecast-time-1">
-          <p> 14:00 </p>
+          <p> <?php echo "$time_2:00" ?> </p>
         </div>
         <div class="weather-forecast-time-2">
-          <p> 16:00 </p>
+          <p> <?php echo "$time_3:00" ?> </p>
         </div>
         <div class="weather-forecast-time-3">
-          <p> 18:00 </p>
+          <p> <?php echo "$time_4:00" ?> </p>
         </div>
         <div class="weather-forecast-time-4">
-          <p> 20:00 </p>
+          <p> <?php echo "$time_5:00" ?> </p>
         </div>
       </div>
     </div>
@@ -70,10 +76,10 @@
 
     <div class="time-calendar-container">
       <div class="time">
-        <p> 13:37 </p>
+        <p> <?php print date('H:i'); ?> </p>
       </div>
       <div class="date">
-        <p> 22 November </p>
+				<p> <?php print date('l'); ?><br> <?php print date('j F') ; ?> </p>
       </div>
       <div class="schedule">
         <p> The Schedule with items </p>
